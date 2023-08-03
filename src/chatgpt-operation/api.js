80a4b65d280853072b344a213c952f57ef3783db
Brainslug <tim@brainslug.nl>
2023-08-03 20:01:58 +0200

    undid accidental reformat from merged PR and removed debug logs

--- src/chatgpt-operation/api.js
+++ src/chatgpt-operation/api.js
@@ -1,8 +1,8 @@
-import { Configuration, OpenAIApi } from "openai"
+import { Configuration, OpenAIApi } from "openai";
 
-import { defineOperationApi } from "@directus/extensions-sdk"
-import { getSetting } from "../lib/util"
-import { openAIField } from "../configuration/fields"
+import { defineOperationApi } from "@directus/extensions-sdk";
+import { getSetting } from "../lib/util";
+import { openAIField } from "../configuration/fields";
 
 export default defineOperationApi({
 	id: "chatgpt-operation",
@@ -18,13 +18,13 @@ export default defineOperationApi({
 		},
 		{ services, database, getSchema }
 	) => {
-		const { SettingsService } = services
-		const schema = await getSchema()
-		const settings = new SettingsService({ schema, knex: database })
+		const { SettingsService } = services;
+		const schema = await getSchema();
+		const settings = new SettingsService({ schema, knex: database });
 
-		const apiKey = await getSetting(settings, openAIField.field, api_key)
-		const configuration = new Configuration({ apiKey })
-		const openai = new OpenAIApi(configuration)
+		const apiKey = await getSetting(settings, openAIField.field, api_key);
+		const configuration = new Configuration({ apiKey });
+		const openai = new OpenAIApi(configuration);
 
 		try {
 			const completion = await openai.createChatCompletion({
@@ -35,19 +35,16 @@ export default defineOperationApi({
 				top_p,
 				frequency_penalty,
 				presence_penalty,
-			})
-
-			const response = completion.data.choices[0].message.content
+			});
+			const response = completion.data.choices[0].message.content;
 
 			return {
 				response: response,
 				usage: completion.data.usage,
 				finish_reason: completion.data.choices[0].finish_reason,
-			}
+			};
 		} catch (err) {
-			console.error(messages)
-			console.log(err)
-			throw err
+			throw err;
 		}
 	},
-})
+});
